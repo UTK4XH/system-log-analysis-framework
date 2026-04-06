55e4603a581a50c2b06be820214567c759733017
UTK4XH <91787180+UTK4XH@users.noreply.github.com>
2026-04-06 11:35:24 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 from log_parser import load_logs
 from analyzer import count_errors, find_error_patterns, detect_anomalies, plot_errors
 
+
 def main():
+    # Load log data
     df = load_logs("../logs/sample_logs.csv")
 
+    # Total error count
     total_errors = count_errors(df)
     print(f"Total Errors: {total_errors}")
 
+    # Error patterns
     error_patterns = find_error_patterns(df)
     print("\nError Patterns:\n", error_patterns)
 
+    # Warnings (anomalies)
     warnings = detect_anomalies(df)
     print("\nWarnings:\n", warnings)
 
-    # 🔥 ADD THIS LINE (IMPORTANT)
+    # Plot graph
     plot_errors(error_patterns)
+
 
 if __name__ == "__main__":
     main()
